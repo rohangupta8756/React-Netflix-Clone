09eb88866eb65a72613475fc199dfbe16aefcd05
rohangupta8756 <120ee0686@nitrkl.ac.in>
2022-07-03 16:08:17 +0530

    Adding Files to the Netflix Clone Repository

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY  = '5db22ccc15d9fef7a77edce28e4a9814';
+// 25e011cfcbb906d6fd8b036d50887d08
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=123`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+};
+ 
+export default requests;
